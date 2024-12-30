7659a9e1c1ab0cb11db6bff8fb9fc488d2af0fd2
auniazirah <yoniesaige@gmail.com>
2024-12-30 23:39:59 +0800

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Simple Notification Example
+// Simple Notification Example 
+// guna code bawah ni untuk functionkan die
+// <script src="script.js"></script>
 window.onload = function() {
     alert('Welcome to My Website!');
 }
+
